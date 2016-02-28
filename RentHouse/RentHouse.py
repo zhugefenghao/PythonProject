41f00e50470828848dcf29823b6ba5f4a4ea5153
zhugefenghao <710228365@qq.com>
2016-02-28 16:10:07 +0800

    change to MultiThread
    change to MultiThread to make it more efficent and use torndb (component
    of tornado, based on mysql) instead of MySQLdb

--- RentHouse/RentHouse.py
+++ RentHouse/RentHouse.py
@@ -1,27 +1,18 @@
 __author__ = 'WJJ'
 import requests
 from bs4 import BeautifulSoup
-import MySQLdb
+import Queue
+import threading
+import torndb
 
 class DB:
-
     def __init__(self):
-        try:
-            self.conn = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user="root",
-                passwd="1234",
-                db="rent_house",
-                charset="utf8"
-            )
-            self.cur = self.conn.cursor()
-        except Exception, data:
-            print "connect database failed, %s" % data
-
-    def __del__(self):
-        self.cur.close()
-        self.conn.close()
+        self.conn = torndb.Connection(
+            "localhost",
+            "rent_house",
+            user="root",
+            password="1234",
+        )
 
     def save(self, obj):
         key_string = ""
@@ -33,11 +24,12 @@ class DB:
             value_string += "'" + value + "',"
         key_string = "(" + key_string.strip(",") + ")"
         value_string = "(" + value_string.strip(",") + ")"
-        sql = "insert into house" + key_string + " VALUES" + value_string
+        if isinstance(obj, House):
+            sql = "insert into house" + key_string + " VALUES" + value_string
         # print sql
-        print value_string
-        self.cur.execute(sql)
-        self.conn.commit()
+        # print value_string
+        self.conn.execute(sql)
+
 
 class RentHouse:
     def __init__(self, url):
@@ -54,7 +46,7 @@ class RentHouse:
 
 
 class House:
-    def __init__(self, pic_title, href, pic_preview, content_title, content_location, content_rent_type, content_area, content_decoration, content_floor, content_direction, content_price, content_house_type):
+    def __init__(self, pic_title, href, pic_preview, content_title, content_location, content_rent_type, content_area, content_extra, content_price, content_house_type):
         self.pic_title = pic_title
         self.href = href
         self.pic_preview = pic_preview
@@ -62,45 +54,65 @@ class House:
         self.content_location = content_location
         self.content_rent_type = content_rent_type
         self.content_area = content_area
-        self.content_decoration = content_decoration
-        self.content_floor = content_floor
-        self.content_direction = content_direction
+        # self.content_decoration = content_decoration
+        # self.content_floor = content_floor
+        # self.content_direction = content_direction
+        self.content_extra = content_extra
         self.content_price = content_price
         self.content_house_type = content_house_type
 
-for i in range(1, 51):
+
+class CrawlerThread(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            url = self.queue.get()
+            print "Proccess: " + url
+            house_items = RentHouse(url).getHouseItems()
+            houses_list = []
+            for item in house_items:
+                pic_part = item.dt
+                content_part = item.dd
+                house = House(None, None, None, None, None, None, None, None, None, None)
+                house.pic_title = pic_part.a["title"]
+                house.href = pic_part.a["href"]
+                try:
+                    house.pic_preview = pic_part.a.img["src2"]
+                except Exception, e:
+                    house.pic_preview = "NULL"
+                house.content_title = content_part.find("p", class_="title").a.string.strip()
+                # if len(content_part.find("p", class_="gray6 mt12").findAll("a")) ==2:
+                #     house.content_location = content_part.find("p", class_="gray6 mt12").findAll("a")[0].span.string + " " +\
+                #                          content_part.find("p", class_="gray6 mt12").findAll("a")[1].span.string + " " +\
+                #                         content_part.find("p", class_="gray6 mt12").findAll("span")[2].string
+                # else:
+                location_string = ""
+                for i in range(len(content_part.find("p", class_="gray6 mt12").findAll("span"))):
+                    location_string += content_part.find("p", class_="gray6 mt12").findAll("span")[i].string + "|"
+                house.content_location = location_string
+                extra_part = house.content_rent_type = content_part.find("p", class_="gray6 mt10").contents
+                house.content_rent_type = extra_part[0].strip()
+                house.content_area = extra_part[2].strip()
+                extra_string = ""
+                for i in range(3, len(extra_part)):
+                    extra_string += extra_part[i].string.strip()
+                house.content_extra = extra_string
+                house.content_price = content_part.find("div", class_="moreInfo").find("p", class_="mt5 alignR").span.string.strip()
+                house.content_house_type = content_part.find("div", class_="moreInfo").find("p", class_="alignR mt8").string.strip()
+                houses_list.append(house)
+            for house in houses_list:
+                db = DB()
+                db.save(house)
+
+queue = Queue.Queue()
+for i in range(1, 1000):
     page = 30 + i
     url = "http://zu.xian.fang.com/house/i" + str(page)
-    print "PROCCESSING:  " + url
-    house_items = RentHouse(url).getHouseItems()
-    houses_list = []
-    for item in house_items:
-        pic_part = item.dt
-        content_part = item.dd
-        house = House(None, None, None, None, None, None, None, None, None, None, None, None)
-        house.pic_title = pic_part.a["title"]
-        house.href = pic_part.a["href"]
-        house.pic_preview = pic_part.a.img["src2"]
-        house.content_title = content_part.find("p", class_="title").a.string.strip()
-        house.content_location = content_part.find("p", class_="gray6 mt12").findAll("a")[0].span.string + " " +\
-                                 content_part.find("p", class_="gray6 mt12").findAll("a")[1].span.string + " " +\
-                                 content_part.find("p", class_="gray6 mt12").findAll("a")[2].span.string + " " +\
-                                content_part.find("p", class_="gray6 mt12").findAll("span")[3].string
-        extra_part = house.content_rent_type = content_part.find("p", class_="gray6 mt10").contents
-        house.content_rent_type = extra_part[0].strip()
-        house.content_area = extra_part[2].strip()
-        if len(extra_part) == 7:
-            house.content_floor = extra_part[4].strip()
-            house.content_direction = extra_part[6].strip()
-        elif len(extra_part) == 9:
-            house.content_decoration = extra_part[4].strip()
-            house.content_floor = extra_part[6].strip()
-            house.content_direction = extra_part[8].strip()
-        house.content_price = content_part.find("div", class_="moreInfo").find("p", class_="mt5 alignR").span.string.strip()
-        house.content_house_type = content_part.find("div", class_="moreInfo").find("p", class_="alignR mt8").string.strip()
-        houses_list.append(house)
+    queue.put(url)
+for i in range(10):
+    CrawlerThread(queue).start()
 
-    db = DB()
-    for house in houses_list:
-        db.save(house)
 
